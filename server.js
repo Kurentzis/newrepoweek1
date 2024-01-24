c86f47a79b0052ca86756bd2fd372f440262d65d
Oleg Lasukov <lasukov2016@mail.ru>
2024-01-24 20:42:55 +0500

    small changes

--- server.js
+++ server.js
@@ -6,7 +6,6 @@
  * Require Statements
  *************************/
 const express = require("express")
-const path = require('path');
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
@@ -22,7 +21,7 @@ app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
 
-app.use(express.static(path.join(__dirname, 'public')));
+
 /* ***********************
  * Routes
  *************************/
