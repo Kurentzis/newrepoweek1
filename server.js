b90ac619c614dfcc8744dacb2bf560a327d50303
Oleg Lasukov <lasukov2016@mail.ru>
2024-01-24 19:52:59 +0500

    add: basic functionality. Styles for inventory is needed

--- server.js
+++ server.js
@@ -6,15 +6,35 @@
  * Require Statements
  *************************/
 const express = require("express")
+const path = require('path');
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRout")
 
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+app.use(express.static(path.join(__dirname, 'public')));
 /* ***********************
  * Routes
  *************************/
 app.use(static)
 
+
+//Index route
+// app.get("/", function(req, res) {
+//   res.render('index', {title: "Home"})
+// })
+app.get("/", baseController.buildHome)
+app.use("/inv", inventoryRoute)
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
